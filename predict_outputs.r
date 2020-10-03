b944d03934d03c9772dddf0001b913bff51329e2
rodolfoksveiga <rodolfoksveiga@gmail.com>
2020-10-03 17:17:20 -0300

    add guide.rmd

--- predict_outputs.r
+++ predict_outputs.r
@@ -1,77 +1,82 @@
 # setup environment ####
+# load libraries
 library(caret)
 library(dplyr)
 library(purrr)
+# load predictive models
+models = readRDS('./result/models.rds')
 
-# load predictive models ####
-load('result/models.rdata')
-
-# load('./result/models_uni_uh_')
-# mls$uni$cgtt = models$xgbt
-# load('./result/models_uni_uh_')
-# mls$uni$dif_cgtt = models$xgbt
-# load('./result/models_multi_uh_')
-# mls$multi$cgtt = models$xgbt
-# load('./result/models_multi_uh_')
-# mls$multi$dif_cgtt = models$xgbt
-# 
-# models = mls
-# rm(mls)
-PredictData = function(targ, dummies, tipo, models) {
+# define functions ####
+# predict one output
+PredOutput = function(targ, dummies, typo, mls) {
+  # targ: the target name ('phft', 'dif_phft', 'cgtt' or 'dif_cgtt)
+  # dummies: the input data after the dummy transformation
+  # typo: the typology ('uni' or 'multi')
+  # mls: list of predictive
+  # associate the target and typology with the related model and execute the predictions
   out = capture.output({
-    predictions = models %>%
-      pluck(tipo, targ) %>%
+    predictions = mls %>%
+      pluck(typo, targ) %>%
       predict(newdata = dummies)
   })
+  # return the predictions
   return(predictions)
 }
-
-PredPerfUH = function(inputs, tbl = NULL, models = models) {
+# predict dweling performance
+PredPerfUH = function(inputs, mls = models) {
   # inputs: list of inputs/predictors
   # tbl: table (csv) file with unlimited rows of predictors
-  # models: compilation of models to predict phft and cgtt for single or multifamily dwelings
-  
-  # test
-  df = read.csv('data_uni_uh.csv')
-  inputs = list(
-    area = 77.16,
-    ratio = c(0.5, 1),
-    pd = 2.5,
-    azi = c(90, 180),
-    comp = 'ref',
-    vid = 'simples_fs87',
-    abs = c(60, 80),
-    paf = 30,
-    fv = 0.45,
-    ven = 'on',
-    somb = 50,
-    tbsm = 17.38078
-  )
-  tbl = NULL
-  
+  # mls: list of predictive models
   # fit inputs into a grid data frame if no table was provided
-  if (is.null(tbl)) {
+  if (is.list(inputs)) {
     inputs = expand.grid(inputs)
+  } else if (is.character(inputs)) {
+    inputs = read.csv(inputs)
+  } else {
+    stop(paste0('Confira se o argumento "inputs" foi associado a uma lista com as ',
+                'variáveis de entrada e os seus valores ou o caminho da tabela/csv ',
+                'na sua máquina.  Qualquer valor diferente disso não é aceito.'))
   }
   # check if the number of inputs is correct and define the typology
   ncols = ncol(inputs)
   if (ncols == 12) {
-    message('Inputs referentes à tipologia unifamiliar.')
-    tipo = 'uni'
+    message('Variáveis de entrada referentes à tipologia unifamiliar.')
+    typo = 'uni'
   } else if (ncols == 14) {
-    message('Inputs referentes à tipologia multifamiliar.')
-    tipo = 'multi'
+    message('Variáveis de entrada referentes à tipologia multifamiliar.')
+    typo = 'multi'
   } else {
-    stop('Reveja seus inputs, pois eles parecem incorretos.')
+    stop('Reveja as suas variáveis de entrada. Me parece que elas estão incorretas..!')
   }
   # create dummy variables
-  dummies = models[[c(tipo, 'dummy_model')]] %>%
+  dummies = mls[[c(typo, 'dummy_model')]] %>%
     predict(newdata = mutate(inputs, targ = NA)) %>%
     data.frame()
   # predict the outputs
   targs = c('phft', 'dif_phft', 'cgtt', 'dif_cgtt')
-  predictions = c('phft', 'dif_phft') %>%
-    sapply(PredictData, dummies, tipo, models)
-  # bind inputs and outputs to build the final data frame
-  data = cbind(inputs, predictions)
+  predictions = targs %>%
+    sapply(PredOutput, dummies, typo, mls) %>%
+    as.data.frame() %>%
+    round(1)
+  # pos-process
+  if (nrow(inputs) == 1) {
+    # adjust the data into proper dataframe
+    data = predictions %>%
+      rename(prediction = '.') %>%
+      t() %>%
+      as.data.frame()
+  } else {
+    # bind inputs and outputs to build the final data frame
+    data = cbind(inputs, predictions)
+  }
+  # return the data
+  return(data)
 }
+
+# application examples ####
+  # complete the code below in order to predict your input data
+inputs = list()
+inputs = ''
+data = PredPerfUH()
+data_path
+write.csv(data, data_path)
